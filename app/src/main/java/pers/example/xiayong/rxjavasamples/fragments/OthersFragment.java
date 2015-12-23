e56c29988b799122b0c42e6b53615ca8040e44fa
THEONE10211024 <xiayong10211024@163.com>
2015-12-23 19:38:42 +0800

    RxJava处理耗时操作

---
+++ app/src/main/java/pers/example/xiayong/rxjavasamples/fragments/OthersFragment.java
@@ -0,0 +1,24 @@
+package pers.example.xiayong.rxjavasamples.fragments;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pers.example.xiayong.rxjavasamples.R;
+
+
+public class OthersFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_others, container, false);
+    }
+
+}
